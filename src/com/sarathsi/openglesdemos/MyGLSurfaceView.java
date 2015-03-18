b7e4189479b714e697e3ef8c460cee4ff530a04b
sarathsi <sarath.singapati.fi@gmail.com>
2015-03-18 23:23:01 +0200

    separate Renderer class

--- src/com/sarathsi/openglesdemos/MyGLSurfaceView.java
+++ src/com/sarathsi/openglesdemos/MyGLSurfaceView.java
@@ -15,11 +15,7 @@
  */
 package com.sarathsi.openglesdemos;
 
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
-
 import android.content.Context;
-import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.util.Log;
 
@@ -29,7 +25,8 @@ import android.util.Log;
  * interacting with drawn objects.
  */
 public class MyGLSurfaceView extends GLSurfaceView {
-
+	private static final String TAG = "MyGLSurfaceView";
+	
     private final MyGLRenderer mRenderer;
 
     public MyGLSurfaceView(Context context) {
@@ -47,35 +44,6 @@ public class MyGLSurfaceView extends GLSurfaceView {
         // Others: RENDERMODE_CONTINUOUSLY
         
         
-    }
-
-    
-    public class MyGLRenderer implements GLSurfaceView.Renderer {
-    	private static final String TAG = "MyGLRenderer";
-
-		@Override
-		public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-			Log.i(TAG, "onSurfaceCreated()");
-	        // specify clear values for the color buffers (background frame color)
-	        GLES20.glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
-		}
-
-		@Override
-		public void onSurfaceChanged(GL10 gl, int width, int height) {
-			Log.i(TAG, "onSurfaceChanged() " + width + "x" + height);
-			GLES20.glViewport(100, 100, width, height);
-			
-		}
-
-		@Override
-		public void onDrawFrame(GL10 gl) {
-			Log.i(TAG, "onDrawFrame()");
-	        // Redraw background color (clear buffers to preset values)
-	        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-	        //(GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_ACCUM_BUFFER_BIT, and GL_STENCIL_BUFFER_BIT)
-
-		}
-    	
     }
 
 }
---
+++ src/com/sarathsi/openglesdemos/shapes/Triangle.java
@@ -0,0 +1,5 @@
+package com.sarathsi.openglesdemos.shapes;
+// 
+public class Triangle {
+
+}
